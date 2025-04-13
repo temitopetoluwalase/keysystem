79285c3ca2437c3abdfe5f1bdcfe190e95b98ce4
TOLUWALASE <68949631+temitopetoluwalase@users.noreply.github.com>
2025-04-13 01:29:02 +0100

    Update content layout secondary background

--- src/pages/home/components/content-layout/contentlayout.jsx
+++ src/pages/home/components/content-layout/contentlayout.jsx
@@ -5,7 +5,7 @@ const ContentLayout = ({ isPrimary, children, noBg }) => {
     <div
       className='flex justify-center py-10 px-2'
       style={{
-        background: noBg ? '' : isPrimary ? '#03020a' : '#020412',
+        background: noBg ? '' : isPrimary ? '#03020a' : '#000015',
       }}
     >
       <div className='max-w-[1234px] w-full'>{children}</div>
